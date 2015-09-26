b468ccca4bede3c9ccb69092d5524c81ca6f0107
toydev <toy@ownway.info>
2015-09-26 12:58:09 +0900

    NavigationDrawer のサンプルを追加した。

---
+++ NavigationDrawer/app/src/main/java/info/ownway/android/example/navigationdrawer/MainActivity.java
@@ -0,0 +1,15 @@
+package info.ownway.android.example.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
