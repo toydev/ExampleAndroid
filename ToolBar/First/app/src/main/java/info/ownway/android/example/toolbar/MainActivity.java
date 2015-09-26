565879ed012514a3d420c00e3dfc1e0ac0b55e27
toydev <toy@ownway.info>
2015-09-26 14:58:10 +0900

    パッケージ名の修正等を行った。

--- ToolBar/First/app/src/main/java/info/ownway/android/example/toolbar/MainActivity.java
+++ ToolBar/First/app/src/main/java/info/ownway/android/example/toolbar/MainActivity.java
@@ -1,4 +1,4 @@
-package example.android.ownway.info.toolbar;
+package info.ownway.android.example.toolbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
 }
