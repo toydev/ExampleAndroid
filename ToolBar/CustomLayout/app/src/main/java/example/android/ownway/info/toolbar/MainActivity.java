300d1d1d09ea59e875824366beeb0692f1b05895
toydev <toy@ownway.info>
2015-09-26 11:31:10 +0900

    ToolBar のカスタムレイアウト用サンプルを追加した。

---
+++ ToolBar/CustomLayout/app/src/main/java/example/android/ownway/info/toolbar/MainActivity.java
@@ -0,0 +1,38 @@
+package example.android.ownway.info.toolbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_menu1:
+                return true;
+            case R.id.action_menu2:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
