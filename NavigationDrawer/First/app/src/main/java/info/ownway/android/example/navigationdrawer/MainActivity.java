82515020483e8b1f7c24105f43e4253d89e5bbbd
toy <toy@ownway.info>
2017-03-30 20:55:54 +0900

    最新の Android Studio で作り直した。

--- NavigationDrawer/First/app/src/main/java/info/ownway/android/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/First/app/src/main/java/info/ownway/android/example/navigationdrawer/MainActivity.java
@@ -1,15 +1,12 @@
 package info.ownway.android.example.navigationdrawer;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
