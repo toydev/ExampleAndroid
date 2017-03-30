7ed4048bd031c30a76c0b2659afd533f85baee2a
toy <toy@ownway.info>
2017-03-30 22:20:32 +0900

    作り直した。

--- NavigationDrawer/WithToolBar/app/src/main/java/info/ownway/android/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/WithToolBar/app/src/main/java/info/ownway/android/example/navigationdrawer/MainActivity.java
@@ -1,31 +1,31 @@
 package info.ownway.android.example.navigationdrawer;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import info.ownway.android.example.navigationdrawer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle m_drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationIcon(R.mipmap.ic_launcher);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
-        drawer.setDrawerListener(new ActionBarDrawerToggle(
-                this, drawer, toolbar,
+        binding.drawer.addDrawerListener(m_drawerToggle = new ActionBarDrawerToggle(
+                this, binding.drawer, binding.toolbar,
                 R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close
         ));
+        m_drawerToggle.syncState();
 
         // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
-
 }
